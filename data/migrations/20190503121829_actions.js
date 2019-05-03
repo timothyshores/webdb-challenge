f5d7d647e99816f03bb710b3939a14454b3d4142
Timothy Shores <timothymshores@gmail.com>
2019-05-03 12:26:16 -0500

    Update data/migrations/20190503121829_actions.js to include a unique id,description, notes, completed status and foreign key product_id

--- data/migrations/20190503121829_actions.js
+++ data/migrations/20190503121829_actions.js
@@ -1,8 +1,21 @@
-
-exports.up = function(knex, Promise) {
-  
+exports.up = function (knex, Promise) {
+    return knex.schema
+        .createTable('actions', tbl => {
+            tbl.increments();
+            tbl.string('description', 128).notNullable();
+            tbl.string('notes', 255);
+            tbl.boolean('completed').notNullable();
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onDelete('RESTRICT')
+                .onUpdate('CASCADE');
+        })
 };
 
-exports.down = function(knex, Promise) {
-  
+exports.down = function (knex, Promise) {
+    return knex.schema
+        .dropTableIfExists('actions')
 };
