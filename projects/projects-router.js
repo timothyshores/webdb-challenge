17adb81d7a08f802edcd1840773b6a5aa99fd5e3
Timothy Shores <timothymshores@gmail.com>
2019-05-03 12:47:38 -0500

    Installed nodemon as dev dependency via yarn add nodemon --dev. Create POST request endpoint to /api/projects/

--- projects/projects-router.js
+++ projects/projects-router.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
+const Projects = require('./projects-model');
+
+router.post('/', (req, res) => {
+    const { name, description } = req.body;
+    const message400 = { error: "Please provide name for the project" }
+    const message500 = { error: "There was an error while saving the project to the database" };
+
+
+    if (name && description) {
+        Projects.addProject({ name, description, completed: false})
+            .then(project => { res.status(201).json(project) })
+            .catch(err => { res.status(500).json(message500) })
+    }
+    else {
+        res.status(400).json(message400);
+    }
+});
 
 module.exports = router;
